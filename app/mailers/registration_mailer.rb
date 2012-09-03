1f3bd00673ee5b331d12e4fcfb035266ae7c20a6
James Grant <jamesgrant1993@yahoo.com>
2012-09-03 23:50:07 +0100

    Created new version of homepage, this is a basic view that can and will be imporved upon

---
+++ app/mailers/registration_mailer.rb
@@ -0,0 +1,15 @@
+class RegistrationMailer < ActionMailer::Base
+  default from: "nuclearflamesweb-djwebsite@yahoo.com"
+
+EMAIL_VERIFICATION_URL = 'http://djwebsite.herokuapp.com/users/new?code='
+  
+	def registration_email(user)
+		@user = user
+		@url = EMAIL_VERIFICATION_URL + user.id.to_s + '_' + user.create_digest(user.id.to_s)
+		mail(	
+			:to => @user.email,
+			:from => "nuclearflamesweb-djwebsite@yahoo.com",
+			:subject => "Please Activate your account",
+		)
+	end
+end
